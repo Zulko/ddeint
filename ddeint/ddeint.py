f5375aa5ac353627b47f83c623f65261b3aa98d6
Zulko <valentin.zulkower@gmail.com>
2019-09-05 16:32:22 +0100

    applied Black code formatting

--- ddeint/ddeint.py
+++ ddeint/ddeint.py
@@ -16,31 +16,32 @@ class ddeVar:
     Very convenient for the integration of DDEs.
     """
 
-    def __init__(self,g,tc=0):
+    def __init__(self, g, tc=0):
         """ g(t) = expression of Y(t) for t<tc """
 
         self.g = g
-        self.tc= tc
+        self.tc = tc
         # We must fill the interpolator with 2 points minimum
         self.itpr = scipy.interpolate.interp1d(
-            np.array([tc-1,tc]), # X
-            np.array([self.g(tc),self.g(tc)]).T, # Y
-            kind='linear', bounds_error=False,
-            fill_value = self.g(tc))
+            np.array([tc - 1, tc]),  # X
+            np.array([self.g(tc), self.g(tc)]).T,  # Y
+            kind="linear",
+            bounds_error=False,
+            fill_value=self.g(tc),
+        )
 
-    def update(self,t,Y):
+    def update(self, t, Y):
         """ Add one new (ti,yi) to the interpolator """
 
         self.itpr.x = np.hstack([self.itpr.x, [t]])
-        Y2 = Y if (Y.size==1) else np.array([Y]).T
+        Y2 = Y if (Y.size == 1) else np.array([Y]).T
         self.itpr.y = np.hstack([self.itpr.y, Y2])
         self.itpr.fill_value = Y
 
-    def __call__(self,t=0):
+    def __call__(self, t=0):
         """ Y(t) will return the instance's value at time t """
 
-        return (self.g(t) if (t<=self.tc) else self.itpr(t))
-
+        return self.g(t) if (t <= self.tc) else self.itpr(t)
 
 
 class dde(scipy.integrate.ode):
@@ -50,27 +51,26 @@ class dde(scipy.integrate.ode):
     integration step.
     """
 
-    def __init__(self,f,jac=None):
+    def __init__(self, f, jac=None):
+        def f2(t, y, args):
+            return f(self.Y, t, *args)
 
-        def f2(t,y,args):
-            return f(self.Y,t,*args)
-        scipy.integrate.ode.__init__(self,f2,jac)
+        scipy.integrate.ode.__init__(self, f2, jac)
         self.set_f_params(None)
 
     def integrate(self, t, step=0, relax=0):
 
-        scipy.integrate.ode.integrate(self,t,step,relax)
-        self.Y.update(self.t,self.y)
+        scipy.integrate.ode.integrate(self, t, step, relax)
+        self.Y.update(self.t, self.y)
         return self.y
 
-    def set_initial_value(self,Y):
+    def set_initial_value(self, Y):
 
-        self.Y = Y #!!! Y will be modified during integration
+        self.Y = Y  #!!! Y will be modified during integration
         scipy.integrate.ode.set_initial_value(self, Y(Y.tc), Y.tc)
 
 
-
-def ddeint(func,g,tt,fargs=None):
+def ddeint(func, g, tt, fargs=None):
     """ Solves Delay Differential Equations
 
     Similar to scipy.integrate.odeint. Solves a Delay differential
@@ -135,7 +135,7 @@ def ddeint(func,g,tt,fargs=None):
     """
 
     dde_ = dde(func)
-    dde_.set_initial_value(ddeVar(g,tt[0]))
+    dde_.set_initial_value(ddeVar(g, tt[0]))
     dde_.set_f_params(fargs if fargs else [])
     results = [dde_.integrate(dde_.t + dt) for dt in np.diff(tt)]
-    return np.array( [g(tt[0])] + results)
+    return np.array([g(tt[0])] + results)
--- examples/lotka.py
+++ examples/lotka.py
@@ -1,23 +1,24 @@
 """ Lotka volterra DDE """
 
-from pylab import *
+from pylab import array, linspace, subplots, show
 from ddeint import ddeint
 
 
-def model(Y,t,d):
-    x,y = Y(t)
-    xd,yd = Y(t-d)
-    return array([0.5*x*(1-yd), -0.5*y*(1-xd)])
+def model(Y, t, d):
+    x, y = Y(t)
+    xd, yd = Y(t - d)
+    return array([0.5 * x * (1 - yd), -0.5 * y * (1 - xd)])
 
-g = lambda t : array([1,2])
-tt = linspace(2,30,20000)
 
-fig, ax = subplots(1,figsize=(4,4))
+g = lambda t: array([1, 2])
+tt = linspace(2, 30, 20000)
+
+fig, ax = subplots(1, figsize=(4, 4))
 
 for d in [0, 0.2]:
-    print( "Computing for d=%.02f"%d )
-    yy = ddeint(model,g,tt,fargs=(d,))
+    print("Computing for d=%.02f" % d)
+    yy = ddeint(model, g, tt, fargs=(d,))
     # WE PLOT X AGAINST Y
-    ax.plot(yy[:,0], yy[:,1], lw=2, label='delay = %.01f'%d)
-    
+    ax.plot(yy[:, 0], yy[:, 1], lw=2, label="delay = %.01f" % d)
+
 show()
